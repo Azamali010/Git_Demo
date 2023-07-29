8276c74a1792e08507bc261f26bafb3b401bc42c
Azamali010 <azamali07711@gmail.com>
2023-07-29 20:20:56 +0530

    this is my sec commit

--- app/src/main/java/com/example/up_to_git/MainActivity.kt
+++ app/src/main/java/com/example/up_to_git/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.up_to_git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getData();
+    }
+
+    private fun getData() {
+        Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show()
     }
 }
